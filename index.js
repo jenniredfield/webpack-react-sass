d0371216d6744f9c4a6365ec58a2eb0ebbbb618c
Jen Bird <jenniredfield@hotmail.com>
2020-02-09 21:53:58 +0000

    Added format

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom"
-import App from "./src/components/App";
-import "./src/styles/styles";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './src/components/App';
+import './src/styles/styles.scss';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<App/>, root)
+ReactDOM.render(<App />, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
 const path = require('path');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const isDevelopment = process.env.NODE_ENV === 'development';
-console.log("TCL: isDevelopment", isDevelopment)
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-function getWebpackConfiguration(env) {
-    console.log('env', env)
-    return {
-        entry: './index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'main.bundle.js'
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+
+function getWebpackConfiguration() {
+  return {
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader'
-                    }
-                },
-                {
-                    test: /\.(css|scss)$/,
-                    use: [isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-                },
-                {
-                    test: /\.html$/,
-                    use: [
-                        {
-                            loader: "html-loader"
-                        }
-                    ]
-                }
-            ]
+        {
+          test: /\.(css|scss)$/,
+          use: [isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
         },
-        plugins: [
-            new HtmlWebPackPlugin({
-                template: "./src/index.html",
-                filename: "./index.html"
-            }),
-            new MiniCssExtractPlugin({
-                filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-                chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-            })
-        ],
-        resolve: {
-            extensions: ['.js', '.jsx', '.scss']
-        }
-    };
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+      }),
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx', '.scss'],
+    },
+  };
 }
 
 module.exports = getWebpackConfiguration();
